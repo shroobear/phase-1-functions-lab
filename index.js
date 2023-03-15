54fc2b0212f5bc468cc454fb62529011a2235907
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-03-15 10:55:34 -0400

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber) {
+    return streetNumber <= 42 ? (42 - streetNumber) : (streetNumber - 42)
+}
+
+function distanceFromHqInFeet(streetNumber) {
+    return (distanceFromHqInBlocks(streetNumber) * 264);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return (start <= destination) ? (destination - start) * 264 : (start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+   let footage = distanceTravelledInFeet(start, destination)
+    if (footage <= 400) {
+        return (0)
+    } else if (footage <= 2000) {
+        return (footage-400)*.02
+    } else if (footage <= 2500) {
+        return (25)
+    } else return 'cannot travel that far'
+}
